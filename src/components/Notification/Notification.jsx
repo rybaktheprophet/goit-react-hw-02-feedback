f6cfd8ed77c7b23c034003e17d5fdfe8cf851f29
Mateusz Rybczynski <113363417+rybaktheprophet@users.noreply.github.com>
2023-07-23 16:56:19 +0200

    all files removed

--- src/components/Notification/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const Notification = ({ message }) => {
-  return <span>{message}</span>;
-};
-
-Notification.propTypes = {
-  message: PropTypes.string,
-};
